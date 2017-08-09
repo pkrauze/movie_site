cdd331427f4cfc37f898e3bc2976d2c86cdeb9df
Paweł Krauze <band.demented@gmail.com>
2017-08-09 12:00:19 +0000

    Decent Exposure Gem + modify Director Controller/Views

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -25,6 +24,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'pg'
+gem 'decent_exposure', '3.0.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,10 +1,11 @@
 class DirectorsController < ApplicationController
-  before_action :set_director, only: [:show, :edit, :update, :destroy]
-
+  
+  expose :director
+  expose :directors, ->{ Director.all }
+  
   # GET /directors
   # GET /directors.json
   def index
-    @directors = Director.all
   end
 
   # GET /directors/1
@@ -14,7 +15,6 @@ class DirectorsController < ApplicationController
 
   # GET /directors/new
   def new
-    @director = Director.new
   end
 
   # GET /directors/1/edit
@@ -24,15 +24,13 @@ class DirectorsController < ApplicationController
   # POST /directors
   # POST /directors.json
   def create
-    @director = Director.new(director_params)
-
     respond_to do |format|
-      if @director.save
-        format.html { redirect_to @director, notice: 'Director was successfully created.' }
-        format.json { render :show, status: :created, location: @director }
+      if director.save
+        format.html { redirect_to director, notice: 'Director was successfully created.' }
+        format.json { render :show, status: :created, location: director }
       else
         format.html { render :new }
-        format.json { render json: @director.errors, status: :unprocessable_entity }
+        format.json { render json: director.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,12 +39,12 @@ class DirectorsController < ApplicationController
   # PATCH/PUT /directors/1.json
   def update
     respond_to do |format|
-      if @director.update(director_params)
-        format.html { redirect_to @director, notice: 'Director was successfully updated.' }
-        format.json { render :show, status: :ok, location: @director }
+      if director.update(director_params)
+        format.html { redirect_to director, notice: 'Director was successfully updated.' }
+        format.json { render :show, status: :ok, location: director }
       else
         format.html { render :edit }
-        format.json { render json: @director.errors, status: :unprocessable_entity }
+        format.json { render json: director.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,21 +52,11 @@ class DirectorsController < ApplicationController
   # DELETE /directors/1
   # DELETE /directors/1.json
   def destroy
-    @director.destroy
+    director.destroy
     respond_to do |format|
       format.html { redirect_to directors_url, notice: 'Director was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_director
-      @director = Director.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def director_params
-      params.require(:director).permit(:firstname, :lastname, :year_of_birth)
-    end
 end
