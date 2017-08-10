602ed5efb0a6f299b393c1188eb86942d43035e4
Paweł Krauze <band.demented@gmail.com>
2017-08-10 13:49:27 +0000

    Add comment-movie association

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,5 @@ class Movie < ActiveRecord::Base
 
     belongs_to :director
     has_many :order_items
+    has_many :comments, as :commentable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810092543) do
+ActiveRecord::Schema.define(version: 20170810134630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170810092543) do
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "directors", force: :cascade do |t|
