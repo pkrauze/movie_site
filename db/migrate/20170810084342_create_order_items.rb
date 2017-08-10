40a52f383d55786855b32ede1c30a85e0b3c4ac3
Paweł Krauze <band.demented@gmail.com>
2017-08-10 08:50:41 +0000

    Migration fixes

--- db/migrate/20170810084342_create_order_items.rb
+++ db/migrate/20170810084342_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-      t.references :product, index: true, foreign_key: true
+      t.references :movie, index: true, foreign_key: true
       t.references :order, index: true, foreign_key: true
       t.decimal :total_price, :precision => 8, :scale => 2
 
