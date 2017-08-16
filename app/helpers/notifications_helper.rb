2cdf8c453197593bb6c05e775ba9064b52a4143a
Paweł Krauze <band.demented@gmail.com>
2017-08-16 00:14:11 +0000

    notifications only after sub

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,6 +3,7 @@ module NotificationsHelper
   def notifications_number
     if user_signed_in?
       @subbed = current_user.subscribers
+      @sub_date = @subbed.last.created_at
       @subbed_dir = @subbed.pluck(:director_id)
       @notif = Notification.where(director_id: @subbed_dir, read: false)
       @notifications = @notif.where("created_at > ?",@sub_date)
