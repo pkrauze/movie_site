241ad66e08ee35403fc733018973f59c90e05aea
Paweł Krauze <band.demented@gmail.com>
2017-08-16 01:24:02 +0000

    clear instance variables

--- app/services/movies/create_movie.rb
+++ app/services/movies/create_movie.rb
@@ -12,8 +12,8 @@ module Movies
         private
         
         def create_notification
-          @movie_id = Movie.last.id
-          Notification.create(director_id: @params[:director_id], movie_id: @movie_id, notification_type:"new movie", read: false).save
+          movie_id = Movie.last.id
+          Notification.create(director_id: @params[:director_id], movie_id: movie_id, notification_type:"new movie", read: false).save
         end
     end
 end
