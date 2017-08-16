7076dcaff44acb649728ed7a0ddffcff6b464914
Paweł Krauze <band.demented@gmail.com>
2017-08-16 01:23:00 +0000

    delete instance variables

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,22 +1,22 @@
 class NotificationsController < ApplicationController
   
   def index
-    @subbed = current_user.subscribers
-    @sub_date = @subbed.last.created_at
-    @subbed_dir = @subbed.pluck(:director_id)
+    subbed = current_user.subscribers
+    sub_date = subbed.last.created_at
+    subbed_dir = subbed.pluck(:director_id)
     
-    @notif = Notification.where(director_id: @subbed_dir, read: false)
-    @allnotif = Notification.where(director_id: @subbed_dir)
+    notif = Notification.where(director_id: subbed_dir, read: false)
+    allnotif = Notification.where(director_id: subbed_dir)
     
-    @notifications = @notif.where("created_at > ?",@sub_date)
-    @allnotifications = @allnotif.where("created_at > ?",@sub_date)
+    @notifications = notif.where("created_at > ?",sub_date)
+    @allnotifications = allnotif.where("created_at > ?",sub_date)
     
   end
   
   def link_through
-    @notification = Notification.find(params[:id])
-    if @notification.update(read:true)
-      redirect_to movie_path(@notification.movie)
+    notification = Notification.find(params[:id])
+    if notification.update(read:true)
+      redirect_to movie_path(notification.movie)
     end
   end
 end
