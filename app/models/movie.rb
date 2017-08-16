470d53eaae2210fe9879e6745ea01208531b3027
Paweł Krauze <band.demented@gmail.com>
2017-08-16 01:27:04 +0000

    add dependent destroy

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,8 +4,8 @@ class Movie < ActiveRecord::Base
     ratyrate_rateable 'visual_effects', 'original_score', 'director', 'custome_design'
 
     belongs_to :director
-    has_many :order_items
+    has_many :order_items, dependent: :destroy
     has_many :comments, as: :commentable
-    has_many :notifications, :dependent => :destroy
+    has_many :notifications, dependent: :destroy
     has_and_belongs_to_many :genres
 end
