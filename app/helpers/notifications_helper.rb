e0f79a2f942fb7f1793f06e01d11d9e52588a7f3
Paweł Krauze <band.demented@gmail.com>
2017-08-16 01:28:33 +0000

    clear instance variables

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,14 +2,14 @@ module NotificationsHelper
     
   def notifications_number
     if user_signed_in?
-      @subbed = current_user.subscribers
-      @sub_date = @subbed.last.created_at
-      @subbed_dir = @subbed.pluck(:director_id)
-      @notif = Notification.where(director_id: @subbed_dir, read: false)
-      @notifications = @notif.where("created_at > ?",@sub_date)
+      subbed = current_user.subscribers
+      sub_date = subbed.last.created_at
+      subbed_dir = subbed.pluck(:director_id)
+      notif = Notification.where(director_id: subbed_dir, read: false)
+      notifications = notif.where("created_at > ?",sub_date)
       
-      if @notifications.size>0
-        "You have #{@notifications.size} new notifications"
+      if notifications.size>0
+        "You have #{notifications.size} new notifications"
       else
         "Notifications"
       end
