45aeec87a13bd1ecf26ad667f1c3f1d0fba17cec
Paweł Krauze <band.demented@gmail.com>
2017-08-18 16:28:16 +0000

    move methods to service object

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,22 +1,7 @@
 class NotificationsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    find_subscribers
-    subbed = current_user.subscribers
-    @notifications = []
-    @allnotifications = []
-    
-    if find_comment_notifications.present?
-      @notifications = find_comment_notifications.where(read: false)
-      @allnotifications = find_comment_notifications.where(read: true)
-    end
-    
-    if subbed.any?
-      if find_sub_notifications.present?
-        @notifications += find_sub_notifications.where(read: false)
-        @allnotifications += find_sub_notifications.where(read: true)
-      end
-    end
+    @notifications,@allnotifications = Notifications::ListNotifications.new(current_user).call
   end
   
   def link_through
@@ -25,28 +10,5 @@ class NotificationsController < ApplicationController
       redirect_to movie_path(notification.movie)
     end
   end
-  
-  private
-  
-  def find_subscribers
-    @subbed = current_user.subscribers
-    if @subbed.any?
-      @sub_date = @subbed.last.created_at
-      @subbed_dir = @subbed.pluck(:director_id)
-      @subbed_genre = @subbed.pluck(:genre_id)
-    end
-  end
-  
-  def find_sub_notifications
-    dir_notif = Notification.where(director_id: @subbed_dir).where("director_id is not ?",nil)
-    genre_notif = Notification.where(genre_id: @subbed_genre).where("genre_id is not ?",nil)
-    
-    @allnotifications = dir_notif.after_sub(@sub_date) + genre_notif.after_sub(@sub_date)
-    
-    return @allnotifications
-  end
-  
-  def find_comment_notifications
-    return @comments_notification = Notification.where("comment_id is not ?",nil).where("user_id = ?",current_user.id)
-  end
+
 end
--- app/services/notifications/list_notifications.rb
+++ app/services/notifications/list_notifications.rb
@@ -1,5 +1,57 @@
 module Notifications
   class ListNotifications
+    def initialize(user)
+      @current_user = user
+    end
     
+    def call
+      find_subscribers
+      find_sub_notifications
+      @notifications = []
+      @allnotifications = []
+      
+      if find_comment_notifications.present?
+        @notifications = find_comment_notifications.where(read: false)
+        @allnotifications = find_comment_notifications.where(read: true)
+      end
+      
+      if subbed.any?
+        if @sub_new_notifications.present? || @sub_all_notifications.present?
+          @notifications += @sub_new_notifications
+          @allnotifications += @sub_all_notifications
+        end
+      end
+      
+      return [@notifications, @allnotifications]
+    end
+    
+    private
+    
+    def find_subscribers
+      if subbed.any?
+        @subdate = subbed.last.created_at
+        @subbed_dir = subbed.pluck(:director_id).compact
+        @subbed_genre = subbed.pluck(:genre_id).compact
+      end
+    end
+    
+    def find_sub_notifications
+      read_dir_notif = Notification.where(director_id: @subbed_dir).after_sub(@subdate)
+      read_genre_notif = Notification.where(genre_id: @subbed_genre).after_sub(@subdate)
+      
+      unread_dir_notif = Notification.where(director_id: @subbed_dir).after_sub(@subdate).unread
+      unread_genre_notif = Notification.where(genre_id: @subbed_genre).after_sub(@subdate).unread
+
+      @sub_new_notifications = unread_dir_notif + unread_genre_notif
+      @sub_all_notifications = read_dir_notif + read_genre_notif
+    end
+    
+    def find_comment_notifications
+      Notification.where("comment_id is not ?",nil).where("user_id = ?",@current_user.id)
+    end
+    
+    def subbed
+      @current_user.subscribers
+    end
   end
 end
