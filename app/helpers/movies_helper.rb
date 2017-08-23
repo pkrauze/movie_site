36f418008e81a2a59376cb0c1b7140c993cfd557
Paweł Krauze <band.demented@gmail.com>
2017-08-23 14:35:09 +0000

    update erb to haml

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,11 +2,19 @@ module MoviesHelper
   
   def item_in_cart(movie)
     @movie_id = movie.id
-    
     if OrderItem.find_by(movie_id: @movie_id)
       true
     else
       false
     end
+  end
+  
+  def list_movie_genres(movie)
+    genres = []
+    movie.genres.each do |g|
+      genres << [g.name.to_s]
+    end
+    
+    return genres.join(', ')
   end
 end
