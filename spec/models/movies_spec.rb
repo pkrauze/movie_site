21c6d2c6dfcbea76398cf2507a94f874ab2630a9
Paweł Krauze <band.demented@gmail.com>
2017-08-30 11:21:29 +0000

    add capybara gem

--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
-describe Movie do
-  context "When testing the movie class" do
-    it "should create the new movie" do
-      m = Movie.new
-      m.title = "Avengers"
-      m.save
-      
-      expect(m.title).to eq("Avengers")
-    end
-    
-    it "should update the last movie" do
-      m = Movie.new
-      m.title = "Avengers"
-      m.save
-      
-      m.update(title:"Indiana Jones")
-      
-      expect(m.title).to eq("Indiana Jones")
-    end
-
-  end
+RSpec.describe Movie, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
