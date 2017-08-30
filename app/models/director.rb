66e89ab2579d97868227655bf7915624c95d65d1
Paweł Krauze <band.demented@gmail.com>
2017-08-30 16:22:14 +0000

    add validations

--- app/models/director.rb
+++ app/models/director.rb
@@ -3,6 +3,8 @@ class Director < ActiveRecord::Base
     has_many :subscribers
     has_many :notifications
     
+    validates_presence_of :firstname, :lastname, :year_of_birth
+    
     def fullname
       self.firstname + " " + self.lastname
     end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,6 @@ class Genre < ActiveRecord::Base
   has_and_belongs_to_many :movies
   has_many :subscribers
   has_many :notifications
+  
+  validates :name, presence: true, uniqueness: true
 end
