d98d80ef9399aa43da85dc1de59365df66d067b5
Paweł Krauze <band.demented@gmail.com>
2017-08-30 16:18:51 +0000

    add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,9 +13,19 @@ class Movie < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :notifications, dependent: :destroy
   has_and_belongs_to_many :genres
+  
+  validates :title, presence: true, uniqueness: true
+  validates_presence_of :desc, :year, :time, director_id, price, images, covers
+  validate :require_at_least_one_genre
 
   def remove_image_file
     FileUtils.rm_rf("#{Rails.root}/public/uploads/movie/images/#{self.id}")
     FileUtils.rm_rf("#{Rails.root}/public/uploads/movie/covers/#{self.id}")
+  end
+  
+  def require_at_least_one_genre
+    if genres.count == 0
+      errors.add_to_base "Please select at least one genre"
+    end
   end
 end
