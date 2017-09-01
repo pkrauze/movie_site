e67eb4f4807c0e14ff5540065bc51482dfaf566a
Paweł Krauze <band.demented@gmail.com>
2017-09-01 11:21:53 +0000

    Add genre images and sub button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901102853) do
+ActiveRecord::Schema.define(version: 20170901111645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170901102853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.json     "images"
   end
 
   create_table "genres_movies", id: false, force: :cascade do |t|
