5e483e3117b2a9761a11aea3d56d6dade7d7e334
pkrauze <band.demented@gmail.com>
2017-09-07 14:12:52 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-# encoding: UTF-8
->>>>>>> 82221bfc01a0eceaad5c11677d9d799418fc7cad
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170901111645) do
 
-<<<<<<< HEAD
   create_table "average_caches", force: :cascade do |t|
     t.integer "rater_id"
     t.string "rateable_type"
@@ -65,58 +60,6 @@ ActiveRecord::Schema.define(version: 20170901111645) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.string "images"
-=======
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "user_id"
-    t.integer  "movie_id"
-  end
-
-  create_table "directors", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.integer  "year_of_birth"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.json     "images"
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
-    t.datetime "created_at"
-  end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
-  create_table "genres", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "slug"
-    t.json     "images"
->>>>>>> 82221bfc01a0eceaad5c11677d9d799418fc7cad
   end
 
   create_table "genres_movies", id: false, force: :cascade do |t|
@@ -129,7 +72,6 @@ ActiveRecord::Schema.define(version: 20170901111645) do
   end
 
   create_table "movies", force: :cascade do |t|
-<<<<<<< HEAD
     t.string "title"
     t.text "desc"
     t.integer "year"
@@ -170,56 +112,11 @@ ActiveRecord::Schema.define(version: 20170901111645) do
 
   create_table "order_statuses", force: :cascade do |t|
     t.string "name"
-=======
-    t.string   "title"
-    t.text     "desc"
-    t.integer  "year"
-    t.integer  "time"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "director_id"
-    t.decimal  "price",       precision: 8, scale: 2
-    t.json     "images"
-    t.string   "slug"
-    t.json     "covers"
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.integer  "director_id"
-    t.integer  "movie_id"
-    t.string   "notification_type"
-    t.boolean  "read"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.integer  "genre_id"
-    t.integer  "comment_id"
-    t.integer  "user_id"
-  end
-
-  add_index "notifications", ["director_id"], name: "index_notifications_on_director_id", using: :btree
-  add_index "notifications", ["movie_id"], name: "index_notifications_on_movie_id", using: :btree
-
-  create_table "order_items", force: :cascade do |t|
-    t.integer  "movie_id"
-    t.integer  "order_id"
-    t.decimal  "total_price", precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.decimal  "unit_price",  precision: 8, scale: 2
-  end
-
-  add_index "order_items", ["movie_id"], name: "index_order_items_on_movie_id", using: :btree
-  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
-
-  create_table "order_statuses", force: :cascade do |t|
-    t.string   "name"
->>>>>>> 82221bfc01a0eceaad5c11677d9d799418fc7cad
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-<<<<<<< HEAD
     t.decimal "total", precision: 8, scale: 2
     t.integer "order_status_id"
     t.datetime "created_at", null: false
@@ -307,101 +204,4 @@ ActiveRecord::Schema.define(version: 20170901111645) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
-=======
-    t.decimal  "total",           precision: 8, scale: 2
-    t.integer  "order_status_id"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-  end
-
-  add_index "orders", ["order_status_id"], name: "index_orders_on_order_status_id", using: :btree
-
-  create_table "overall_averages", force: :cascade do |t|
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "overall_avg",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
-  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.integer  "cacheable_id"
-    t.string   "cacheable_type"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
-
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
-
-  create_table "subscribers", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "director_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "genre_id"
-  end
-
-  add_index "subscribers", ["director_id"], name: "index_subscribers_on_director_id", using: :btree
-  add_index "subscribers", ["user_id"], name: "index_subscribers_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-  end
-
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
-
-  add_foreign_key "notifications", "directors"
-  add_foreign_key "notifications", "movies"
-  add_foreign_key "order_items", "movies"
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "orders", "order_statuses"
-  add_foreign_key "subscribers", "directors"
-  add_foreign_key "subscribers", "users"
->>>>>>> 82221bfc01a0eceaad5c11677d9d799418fc7cad
 end
