17fa06ce02245f694c54c5488e7e537ae0fa91d6
pkrauze <band.demented@gmail.com>
2017-09-07 14:39:32 +0200

    add permit_params

--- app/admin/director.rb
+++ app/admin/director.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Director do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :firstname, :lastname, :yearofbirth, :images
 #
 # or
 #
