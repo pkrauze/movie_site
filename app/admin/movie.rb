c34f1f11e5deca057fcee8f892c69ff9f04239d5
pkrauze <band.demented@gmail.com>
2017-09-07 14:49:13 +0200

    add remove_filters

--- app/admin/movie.rb
+++ app/admin/movie.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Movie do
+	remove_filter :rates, :notifications, :order_items, :comments, :rater
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
