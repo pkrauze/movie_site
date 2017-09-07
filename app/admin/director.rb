43a184e7917b768008261213a30f78525c18b727
pkrauze <band.demented@gmail.com>
2017-09-07 18:11:45 +0200

    add remove_filter

--- app/admin/director.rb
+++ app/admin/director.rb
@@ -2,7 +2,8 @@ ActiveAdmin.register Director do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :firstname, :lastname, :year_of_birth, :images
+	permit_params :firstname, :lastname, :year_of_birth, :images
+	remove_filter :movies, :subscribers, :notifications, :rates_without_dimension, :raters_without_dimension, :rate_average_without_dimension, :director_rating_rates, :director_rating_raters, :images, :slug
 #
 # or
 #
