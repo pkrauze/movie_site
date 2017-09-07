23a61d4b895cf98c1c72ecc339d38de1d1098de4
pkrauze <band.demented@gmail.com>
2017-09-08 01:38:15 +0200

    change paginates_per

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   before_destroy :remove_image_file
 
-  paginates_per 30
+  paginates_per 25
       
   mount_uploaders :images, ImageUploader
   mount_uploaders :covers, CoverUploader
