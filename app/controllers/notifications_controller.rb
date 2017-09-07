a1d9525de1485bad64f54127a47f7dd3ce2fc7e8
Paweł Krauze <band.demented@gmail.com>
2017-09-07 13:51:10 +0200

    Update notifications_controller.rb

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,6 @@
 class NotificationsController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!
-=======
-  before_filter :authenticate_user!
->>>>>>> 82221bfc01a0eceaad5c11677d9d799418fc7cad
+  
   def index
     @notifications,@allnotifications = Notifications::ListNotifications.new(current_user).call
   end
