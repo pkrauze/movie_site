c2349248b91d26dcd1458ecf5a9af3c2efdf4efa
pkrauze <band.demented@gmail.com>
2017-09-08 19:30:06 +0200

    add WaitForAjax helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL
+  config.include WaitForAjax, type: :feature
 end
